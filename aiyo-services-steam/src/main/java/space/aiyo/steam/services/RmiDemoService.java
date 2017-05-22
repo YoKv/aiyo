e8129e790d6440225438b733a949627f9330f280
yo <123>
2017-05-22 17:00:28 +0800

    RMI

---
+++ aiyo-services-steam/src/main/java/space/aiyo/steam/services/RmiDemoService.java
@@ -0,0 +1,11 @@
+package space.aiyo.steam.services;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yo on 2017/5/22.
+ */
+@Component
+public interface RmiDemoService{
+    String serviceDemo();
+}
--- document/api资源搜集.md
+++ document/api资源搜集.md
@@ -1,2 +1,6 @@
 官方文档
 https://wiki.teamfortress.com/wiki/WebAPI#Dota_2
+
+
+rmi rest对比项目
+https://github.com/kamoor/spring-boot-sample
