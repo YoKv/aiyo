1a72867007528044c60789975c2c7868b432d13d
yo <123>
2017-05-27 10:44:15 +0800

    实体修改

--- aiyo-steam-services/src/main/java/space/aiyo/steam/entity/Dota2MatchPlayerEntity.java
+++ aiyo-steam-services/src/main/java/space/aiyo/steam/entity/Dota2MatchPlayerEntity.java
@@ -1,5 +1,7 @@
 package space.aiyo.steam.entity;
 
+import com.alibaba.fastjson.JSONArray;
+
 /**
  * 一场比赛中的选手数据
  * Created by yo on 2017/5/27.
@@ -11,16 +13,41 @@ public class Dota2MatchPlayerEntity {
      * A player's slot is returned via an 8-bit unsigned integer.
      * The first bit represent the player's team, false if Radiant and true if dire.
      * The final three bits represent the player's position in that team, from 0-4.
-     ┌─────────────── Team (false if Radiant, true if Dire).
-     │ ┌─┬─┬─┬─────── Not used.
-     │ │ │ │ │ ┌─┬─┬─ The position of a player within their team (0-4).
-     │ │ │ │ │ │ │ │
-     0 0 0 0 0 0 0 0
-     *
-     *
+     * ┌─────────────── Team (false if Radiant, true if Dire).
+     * │ ┌─┬─┬─┬─────── Not used.
+     * │ │ │ │ │ ┌─┬─┬─ The position of a player within their team (0-4).
+     * │ │ │ │ │ │ │ │
+     * 0 0 0 0 0 0 0 0
      */
     private int player_slot;
     private int hero_id;
+    private int item_0;
+    private int item_1;
+    private int item_2;
+    private int item_3;
+    private int item_4;
+    private int item_5;
+    private int backpack_0;
+    private int backpack_1;
+    private int backpack_2;
+    private int kills;
+    private int deaths;
+    private int assists;
+    private int leaver_status;
+    private int last_hits;
+    private int denies;
+    private int gold_per_min;
+    private int xp_per_min;
+    private int level;
+    private int hero_damage;
+    private int tower_damage;
+    private int hero_healing;
+    private int gold;
+    private int gold_spent;
+    private int scaled_hero_damage;
+    private int scaled_tower_damage;
+    private int scaled_hero_healing;
+    private JSONArray ability_upgrades;
 
     public Dota2MatchPlayerEntity(int account_id, int player_slot, int hero_id) {
         this.account_id = account_id;
@@ -29,133 +56,14 @@ public class Dota2MatchPlayerEntity {
     }
 }
 
-/*
-{
-        "account_id":344441990,
-        "player_slot":0,
-        "hero_id":72,
-        "item_0":63,
-        "item_1":156,
-        "item_2":116,
-        "item_3":108,
-        "item_4":154,
-        "item_5":158,
-        "backpack_0":46,
-        "backpack_1":65,
-        "backpack_2":0,
-        "kills":16,
-        "deaths":7,
-        "assists":26,
-        "leaver_status":0,
-        "last_hits":321,
-        "denies":13,
-        "gold_per_min":679,
-        "xp_per_min":653,
-        "level":25,
-        "hero_damage":44727,
-        "tower_damage":7008,
-        "hero_healing":0,
-        "gold":2735,
-        "gold_spent":28695,
-        "scaled_hero_damage":25061,
-        "scaled_tower_damage":4816,
-        "scaled_hero_healing":0,
-        "ability_upgrades":[
-        {
-        "ability":5651,
-        "time":389,
-        "level":1
-        },
-        {
-        "ability":5257,
-        "time":507,
-        "level":2
-        },
-        {
-        "ability":5651,
-        "time":596,
-        "level":3
-        },
-        {
-        "ability":5257,
-        "time":717,
-        "level":4
-        },
-        {
-        "ability":5651,
-        "time":792,
-        "level":5
-        },
-        {
-        "ability":5067,
-        "time":944,
-        "level":6
-        },
-        {
-        "ability":5651,
-        "time":995,
-        "level":7
-        },
-        {
-        "ability":5257,
-        "time":1047,
-        "level":8
-        },
-        {
-        "ability":5257,
-        "time":1203,
-        "level":9
-        },
-        {
-        "ability":6405,
-        "time":1271,
-        "level":10
-        },
-        {
-        "ability":5363,
-        "time":1366,
-        "level":11
-        },
-        {
-        "ability":5067,
-        "time":1489,
-        "level":12
-        },
-        {
-        "ability":5363,
-        "time":1539,
-        "level":13
-        },
-        {
-        "ability":5363,
-        "time":1550,
-        "level":14
-        },
-        {
-        "ability":5939,
-        "time":1575,
-        "level":15
-        },
-        {
-        "ability":5363,
-        "time":1667,
-        "level":16
-        },
-        {
-        "ability":5067,
-        "time":1893,
-        "level":17
-        },
-        {
-        "ability":6077,
-        "time":2165,
-        "level":18
-        },
-        {
-        "ability":6312,
-        "time":2951,
-        "level":19
-        }
-        ]
+class abilityUpgrades {
+    private int ability;
+    private int time;
+    private int level;
 
-        },*/
+    public abilityUpgrades(int ability, int time, int level) {
+        this.ability = ability;
+        this.time = time;
+        this.level = level;
+    }
+}
