dac5997def8f0476b41e3e6bde025c891473042b
yokv <ez4tzl@hotmail.com>
2017-07-27 09:02:47 +0800

    私人云盘项目

---
+++ personalpan/src/main/java/apce/aiyo/personalpan/PersonalpanApplication.java
@@ -0,0 +1,12 @@
+package apce.aiyo.personalpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalpanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalpanApplication.class, args);
+	}
+}
