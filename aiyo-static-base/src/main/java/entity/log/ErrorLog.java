516ac5980d537539537bb0c4ab7a301c47b3f97b
yo <ez4tzl@hotmail.com>
2017-11-20 09:36:40 +0800

    steam调整

---
+++ aiyo-static-base/src/main/java/entity/log/ErrorLog.java
@@ -0,0 +1,4 @@
+package entity.log;
+
+public class ErrorLog {
+}
---
+++ aiyo-static-base/src/main/java/entity/user/AccountEntity.java
@@ -0,0 +1,12 @@
+package entity.user;
+
+
+public class AccountEntity {
+    private long id;
+    private String mobliePhone;
+    private String username;
+    private String password;
+    private int status;
+    private String addTime;
+
+}
