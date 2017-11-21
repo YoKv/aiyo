e1e0ae5cf504d2d59a051e3b01936bbe880fc8b8
yo <ez4tzl@hotmail.com>
2017-11-21 17:33:32 +0800

    steam调整

---
+++ aiyo-service-steam/src/main/java/space/aiyo/contsant/APIRouterContsant.java
@@ -0,0 +1,13 @@
+package space.aiyo.contsant;
+
+public final class APIRouterContsant {
+    /**
+     * API Route
+     */
+    public static final String API_GET_HERO = "/heroes/:heroId";
+    public static final String API_LIST_HEROES = "/heroes";
+    public static final String API_CREATE_HERO = "/heroes";
+    public static final String API_UPDATE_HERO = "/heroes/:heroId";
+    public static final String API_DELETE_HERO = "/heroes/:heroId";
+    public static final String API_DELETE_ALL_HERO = "/heroes";
+}
