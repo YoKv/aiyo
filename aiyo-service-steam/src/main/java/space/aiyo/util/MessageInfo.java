acf438304d6588e8c962d132facbd0023fe3c4ba
yokv <ez4tzl@hotmail.com>
2018-01-14 17:04:55 +0800

    steam 项目 vertx 重构

--- aiyo-service-steam/src/main/java/space/aiyo/util/MessageInfo.java
+++ aiyo-service-steam/src/main/java/space/aiyo/util/MessageInfo.java
@@ -1,8 +1,25 @@
 package space.aiyo.util;
 
+import space.aiyo.var.EventEnum;
+
 /**
  * CREATE BY Yo ON 2018/1/14 15:30
  */
 public class MessageInfo {
 
+  private EventEnum eventEnum;
+  private Object var;
+
+  public MessageInfo(EventEnum eventEnum, Object var) {
+    this.eventEnum = eventEnum;
+    this.var = var;
+  }
+
+  public EventEnum getEventEnum() {
+    return eventEnum;
+  }
+
+  public Object getVar() {
+    return var;
+  }
 }
--- aiyo-service-steam/src/main/java/space/aiyo/var/EventEnum.java
+++ aiyo-service-steam/src/main/java/space/aiyo/var/EventEnum.java
@@ -4,5 +4,9 @@ package space.aiyo.var;
  * CREATE BY Yo ON 2018/1/14 15:29
  */
 public enum EventEnum {
-
+  INSERT,
+  UPDATE,
+  DELETE,
+  FIND,
+  CRAWLER;
 }
