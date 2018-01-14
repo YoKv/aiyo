90f14f4cb475953146d8cdad13417d7dde9a7a98
yokv <ez4tzl@hotmail.com>
2018-01-14 12:45:30 +0800

    基础数据库

--- aiyo-service-steam/src/main/java/space/aiyo/crud/MongoDBCRUDVerticle.java
+++ aiyo-service-steam/src/main/java/space/aiyo/crud/MongoDBCRUDVerticle.java
@@ -2,18 +2,34 @@ package space.aiyo.crud;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.FindOptions;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.mongo.MongoClientDeleteResult;
+import io.vertx.ext.mongo.MongoClientUpdateResult;
+import io.vertx.ext.mongo.UpdateOptions;
+import java.util.List;
 
 /**
+ * MongoClient透明,便于切换数据源
  * CREATE BY Yo ON 2018/1/14 00:46
  */
 public class MongoDBCRUDVerticle extends AbstractVerticle {
 
   private MongoClient client;
+  private UpdateOptions updateOptions;
   protected String documentName;
+
+  @Override
+  public void init(Vertx vertx, Context context) {
+    super.init(vertx, context);
+    client = MongoClient.createShared(vertx, config());
+    updateOptions = new UpdateOptions().setMulti(true).setUpsert(false);
+  }
 
   @Override
   public JsonObject config() {
@@ -36,66 +52,41 @@ public class MongoDBCRUDVerticle extends AbstractVerticle {
   }
 
   @Override
-  public void start() throws Exception {
-    client = MongoClient.createShared(vertx, config());
+  public void stop() throws Exception {
+    super.stop();
+    client.close();
   }
 
+  protected void insert(JsonObject document, Handler<AsyncResult<String>> handler) {
+    client.insert(documentName, document, handler);
 
-  protected long insert(JsonObject document) {
-    return 0;
   }
 
-
-  protected int insertBatch(JsonArray array) {
-    return 0;
+  protected void insertBatch(JsonArray array) {
+    // TODO: 2018/1/14 命令行
   }
 
-  protected void save(JsonObject document, Handler<AsyncResult<String>> handler) {
-    client.save(documentName, document, handler);
+  protected void update(JsonObject document, JsonObject query,
+      Handler<AsyncResult<MongoClientUpdateResult>> handler) {
+    client.updateCollectionWithOptions(documentName, query, new JsonObject().put("$set", document),
+        updateOptions, handler);
   }
 
-
-  protected int saveBatch(JsonArray array) {
-    return 0;
+  protected void find(JsonObject query, Handler<AsyncResult<List<JsonObject>>> handler) {
+    client.find(documentName, query, handler);
   }
 
-
-  protected int update() {
-    return 0;
+  protected void findWithOptions(JsonObject query, FindOptions findOptions,
+      Handler<AsyncResult<List<JsonObject>>> handler) {
+    client.findWithOptions(documentName, query, findOptions, handler);
+  }
+  
+  protected void delete(JsonObject query,
+      Handler<AsyncResult<MongoClientDeleteResult>> handler) {
+    client.removeDocuments(documentName, query, handler);
   }
 
-
-  protected int updateBatch() {
-    return 0;
-  }
-
-
-  protected int updateByQuery() {
-    return 0;
-  }
-
-
-  protected JsonArray findByQuery() {
-    return null;
-  }
-
-
-  protected JsonObject findOne() {
-    return null;
-  }
-
-
-  protected int delete() {
-    return 0;
-  }
-
-
-  protected int deleteByQuery() {
-    return 0;
-  }
-
-
-  protected int count() {
-    return 0;
+  protected void count(JsonObject query, Handler<AsyncResult<Long>> handler) {
+    client.count(documentName, query, handler);
   }
 }
